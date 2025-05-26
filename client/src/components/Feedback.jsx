6924752b90cb20047e9cc8942442ee7d4cee847a
Joel McCandless <mail@joelmccandless.com>
2025-05-26 14:49:23 -0400

    move feedback to footer in large screens

--- client/src/components/Feedback.jsx
+++ client/src/components/Feedback.jsx
@@ -2,10 +2,8 @@ import '../styles/Feedback.css';
 
 export default function Feedback() {
     return (
-        <div className='feedback'>
-            <p>
-                Have feedback? Please fill out <a href='https://forms.office.com/r/VTC45CRPV3'>our feedback form</a>!
-            </p>
-        </div>
+        <p className='feedback-p'>
+            Have feedback? Please fill out <a href='https://forms.office.com/r/VTC45CRPV3'>our feedback form</a>!
+        </p>
     );
 }
