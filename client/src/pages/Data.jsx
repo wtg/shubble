52d044bfe938b63586033df9d3e877df9d0348c5
Kyra McCandless <mail@kyramccandless.com>
2025-06-15 20:48:47 -0400

    Adding placeholder for map

--- client/src/pages/Data.jsx
+++ client/src/pages/Data.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import "../styles/Data.css"
+import MapKitMap from '../components/MapKitMap';
 
 export default function Data() {
 
@@ -77,6 +78,7 @@ export default function Data() {
 		    ))}
 		</tbody>
 	    </table>
+	    <MapKitMap vehicles={ location } />
 	</>
     );
 }
