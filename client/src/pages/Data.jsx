49eca882aa36a07760ccfd09d6fe8a1ae882c462
Kyra McCandless <mail@kyramccandless.com>
2025-06-16 09:41:32 -0400

    Fixing bug in dropdown menu

--- client/src/pages/Data.jsx
+++ client/src/pages/Data.jsx
@@ -9,29 +9,29 @@ export default function Data() {
 	{id: 0,
          lat: 0,
          lng: 0,
-         timestamp: 0,
+         timestamp: "Timestamp1",
          speed: 0,
          heading: "N",
 	 address: "zero"},
 	{id: 1,
          lat: 1,
          lng: 1,
-         timestamp: 1,
+         timestamp: "Timestamp2",
          speed: 1,
-         heading: "N",
+         heading: "E",
 	 address: "one"},
 	{id: 2,
          lat: 2,
          lng: 2,
-         timestamp: 2,
+         timestamp: "Timestamp3",
          speed: 2,
-         heading: "N",
+         heading: "S",
 	 address: "two"}			  
     ];
 
     const [shuttle, setShuttle] = useState(shuttleList[0]);
-    const handleShuttleChange = (e) => {
-	setShuttle(parseInt(event.target.value));
+    const handleShuttleChange = (event) => {
+	setShuttle(shuttleList[parseInt(event.target.value)]);
     }
 
     
@@ -44,42 +44,39 @@ export default function Data() {
 
 	    <div className = "table-map-sidebyside">
 		<div>
-		<p>
-		    Shuttle:
-		    <select value={shuttle} onChange={handleShuttleChange}>
-			{
-			    shuttleList.map((shuttle, index) =>
-				<option key={index} vlaue={index}>
-				    {shuttle.id}
+		    <p>
+			Shuttle:
+			<select value={shuttle.id} onChange={handleShuttleChange}>
+			    {shuttleList.map(shuttle => (
+				<option key={shuttle.id} value={shuttle.id}>
+				    {shuttle.address}
 				</option>
-			    )
-			}	
-		    </select>
-		</p>
-		<table className = "data-table">
-		    <thead>
-			<tr>
-			    <th>
-				Timestamp
-			    </th>
-			    <th>
-				Location
-			    </th>
-			    <th>
-				Speed
-			    </th>
-			</tr>
-		    </thead>
-		    <tbody>
-			{shuttleList.map((shuttle, index) => (
-			    <tr key={index}>
+			    ))}	
+			</select>
+		    </p>
+
+		    <table className = "data-table">
+			<thead>
+			    <tr>
+				<th>
+				    Timestamp
+				</th>
+				<th>
+				    Location
+				</th>
+				<th>
+				    Speed
+				</th>
+			    </tr>
+			</thead>
+			<tbody>
+			    <tr>
 				<td>{shuttle.timestamp}</td>
 				<td>{shuttle.lat}, {shuttle.lng}</td>
 				<td>{shuttle.speed} mph</td>
 			    </tr>
-			))}
-		    </tbody>
-		</table>
+			</tbody>
+		    </table>
 		</div>
 		<MapKitMap vehicles={ location } />
 	    </div>
