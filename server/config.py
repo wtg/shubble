275521a46750b17e34ecf099c31725fb9a75c4ea
Joel McCandless <mail@joelmccandless.com>
2025-07-05 16:48:58 -0400

    patch db url

--- server/config.py
+++ server/config.py
@@ -9,4 +9,6 @@ class Config:
 
     # database settings
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://', 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
