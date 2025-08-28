eb956159fcd8efeb19a60c4cbcfce3648c94e8d4
Joel McCandless <mail@joelmccandless.com>
2025-08-28 19:30:57 -0400

    send closest route in locations update

--- server/routes.py
+++ server/routes.py
@@ -4,6 +4,7 @@ from .models import Vehicle, GeofenceEvent, VehicleLocation
 from pathlib import Path
 from sqlalchemy import func, and_
 from datetime import datetime, date
+from data.stops import Stops
 import logging
 
 logger = logging.getLogger(__name__)
@@ -69,6 +70,14 @@ def get_locations():
     # Format response
     response = {}
     for loc, vehicle in results:
+        # Get closest loop
+        _, closest_distance, closest_route_name, _ = Stops.get_closest_point(
+            (loc.latitude, loc.longitude)
+        )
+        _, stop_name = Stops.is_at_stop((loc.latitude, loc.longitude))
+        route_name = closest_route_name if closest_distance < 0.0002 else None  # within 20 meters
+        if stop_name == 'STUDENT_UNION':
+            route_name = "UNCLEAR"
         response[loc.vehicle_id] = {
             'name': loc.name,
             'latitude': loc.latitude,
@@ -76,6 +85,7 @@ def get_locations():
             'timestamp': loc.timestamp.isoformat(),
             'heading_degrees': loc.heading_degrees,
             'speed_mph': loc.speed_mph,
+            'route_name': route_name,
             'is_ecu_speed': loc.is_ecu_speed,
             'formatted_location': loc.formatted_location,
             'address_id': loc.address_id,
