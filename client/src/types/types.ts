2d2057be51e174bd3374d109741eb021553512b1
williamschen23 <chenwill2005@gmail.com>
2025-08-30 11:30:15 -0400

    remove types

--- client/src/types/types.ts
+++
@@ -1,8 +0,0 @@
-class TextAnnotation extends window.mapkit.AnnotationView {
-  constructor(annotation) {
-    super(annotation);
-    this.element.style.background = "none";
-    this.element.style.border = "none";
-    this.element.innerText = annotation.title;
-  }
-}
