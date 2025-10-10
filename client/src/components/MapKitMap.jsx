0cc553f845bb29800121b03f23e7949d4d773242
Richard Sun <sunr5@rpi.edu>
2025-10-10 11:38:37 -0400

    fix: make shuttles labeled on page load
    Fixes #181

--- client/src/components/MapKitMap.jsx
+++ client/src/components/MapKitMap.jsx
@@ -372,9 +372,6 @@ export default function MapKitMap({ routeData, vehicles, generateRoutes = false,
         console.log(`Updating vehicle ${key} to ${vehicle.latitude}, ${vehicle.longitude}`);
         vehicleOverlays.current[key].coordinate = coordinate;
         vehicleOverlays.current[key].subtitle = `${vehicle.speed_mph} mph`;
-        if (vehicle.route_name !== "UNCLEAR") {
-          vehicleOverlays.current[key].color = vehicle.route_name ? routeData[vehicle.route_name].COLOR : '#444444';
-        }
       } else {
         // new vehicle: add to map
         console.log(`Adding vehicle ${key} to ${vehicle.latitude}, ${vehicle.longitude}`);
@@ -387,6 +384,10 @@ export default function MapKitMap({ routeData, vehicles, generateRoutes = false,
         });
         map.addAnnotation(annotation);
         vehicleOverlays.current[key] = annotation;
+      }
+
+      if (vehicle.route_name !== "UNCLEAR") {
+        vehicleOverlays.current[key].color = vehicle.route_name ? routeData[vehicle.route_name].COLOR : '#444444';
       }
     });
 
