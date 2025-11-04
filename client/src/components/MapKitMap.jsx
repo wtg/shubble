a06846a5da8738977646239682c6b32f141e3a7f
Ryan Tsang <tsangr2@rpi.edu>
2025-11-04 16:13:33 -0500

    Fixed minor shuttle icon positioning error

--- client/src/components/MapKitMap.jsx
+++ client/src/components/MapKitMap.jsx
@@ -386,7 +386,7 @@ export default function MapKitMap({ routeData, vehicles, generateRoutes = false,
           <!-- Inner white circle -->
           <circle cx="25" cy="25" r="21" fill="white" />
           <!-- Shuttle silhouette -->
-          <g transform="translate(13,13) scale(1)">
+          <g transform="translate(6,6) scale(1.6)">
             <path
               fill="${routeColor}"
               d="M18 11H6V6h12m-1.5 11a1.5 1.5 0 0 1-1.5-1.5a1.5 1.5 0 0 1 1.5-1.5a1.5 1.5 0 0 1 1.5 1.5a1.5 1.5 0 0 1-1.5 1.5m-9 0A1.5 1.5 0 0 1 6 15.5A1.5 1.5 0 0 1 7.5 14A1.5 1.5 0 0 1 9 15.5A1.5 1.5 0 0 1 7.5 17M4 16c0 .88.39 1.67 1 2.22V20a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1h8v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1.78c.61-.55 1-1.34 1-2.22V6c0-3.5-3.58-4-8-4s-8 .5-8 4z"
