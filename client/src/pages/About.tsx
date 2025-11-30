0db85a9d55297132089052ffa4363e33f7833415
Ryan Tsang <tsangr2@rpi.edu>
2025-11-30 14:33:34 -0500

    Fixed mobile view and removed background div

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -22,28 +22,26 @@ export default function About() {
   }, []);
 
   return (
-    <div id ="grad">
-      <div className="about-wrapper">
-        <div className="about-left">
-          <p className="about-small-title">Making Shuttles</p>
-          <h1 className="about-main-header"><span className='word-rotator'>{words[wordIndex]}</span></h1>
-          <div className="about-description">
-            <h1>Track RPI shuttles with live location and view schedules with Shubble.<br /><br /><br /></h1>
-            <p>
-              Shubble is an open source project under the Rensselaer Center for Open Source (RCOS).<br />
-              Have an idea to improve it? Contributions are welcome!<br /> 
-              Visit our <a href='https://github.com/wtg/shubble' target='_blank'>Github Repository</a> to learn more.<br />
-              Interested in Shubble's data? Take a look at our
-              <Link to='/data'>
-                <span className = 'link1'>data page</span>
-              </Link>.
-            </p>
-          </div>
+    <div className="about-wrapper">
+      <div className="about-left">
+        <p className="about-small-title">Making Shuttles</p>
+        <h1 className="about-main-header"><span className='word-rotator'>{words[wordIndex]}</span></h1>
+        <div className="about-description">
+          <h1>Track RPI shuttles with live location and view schedules with Shubble.<br /><br /></h1>
+          <p>
+            Shubble is an open source project under the Rensselaer Center for Open Source (RCOS).<br />
+            Have an idea to improve it? Contributions are welcome!<br /> 
+            Visit our <a href='https://github.com/wtg/shubble' target='_blank'>Github Repository</a> to learn more.<br />
+            Interested in Shubble's data? Take a look at our
+            <Link to='/data'>
+              <span className = 'link1'>data page</span>
+            </Link>.
+          </p>
         </div>
+      </div>
 
-        <div className="about-right">
-          <img src="/RPIStudentUnionv2.png" alt="RPI Student Union" className="about-image" />
-        </div>
+      <div className="about-right">
+        <img src="/RPIStudentUnionv2.png" alt="RPI Student Union" className="about-image" />
       </div>
     </div>
   )
