eb46d299adde78eee3f4e3446fdd744adfffcc0c
williamschen23 <chenwill2005@gmail.com>
2026-01-24 16:21:20 -0500

    test this

--- frontend/src/locations/LiveLocation.tsx
+++ frontend/src/locations/LiveLocation.tsx
@@ -6,12 +6,14 @@ import "./styles/LiveLocation.css";
 import routeData from '../shared/routes.json';
 import type { ShuttleRouteData } from '../types/route';
 import aggregatedSchedule from '../shared/aggregated_schedule.json';
+import type { AggregatedScheduleType } from '../types/schedule';
 
 export default function LiveLocation() {
   // Filter routeData to only include routes present in aggregatedSchedule
   // TODO: figure out how to make this type correct...
+  const rawAggregatedSchedule = aggregatedSchedule as unknown as AggregatedScheduleType;
   const filteredRouteData = Object.fromEntries(
-      Object.entries(routeData).filter(([routeName]) => aggregatedSchedule.some(daySchedule => routeName in daySchedule))
+      Object.entries(routeData).filter(([routeName]) => rawAggregatedSchedule.some(daySchedule => routeName in daySchedule))
     ) as unknown as ShuttleRouteData;
   const [selectedRoute, setSelectedRoute] = useState<string | null>(null);
 
