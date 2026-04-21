b3b961697b11d2b772d219f0de41e199ebd67b1f
Trivika Komatireddy <komatt@rpi.edu>
2026-04-20 21:19:19 -0400

    Removed file allowing different workers to run in same process

--- backend/__main__.py
+++
@@ -1,8 +0,0 @@
-"""Entry point for running the worker as a module."""
-import asyncio
-from backend.locations_worker import run_locations_worker
-from backend.ml_worker import run_ml_worker
-
-if __name__ == "__main__":
-    asyncio.run(run_locations_worker())
-    asyncio.run(run_ml_worker())
